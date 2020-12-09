b90457ad440b566860ee71fa66b17a51b260d89b
PowerPixel <medhy.fournier3@gmail.com>
2020-12-09 17:49:00 +0100

    Initial commit.

---
+++ LeftyFlip.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using OpenTabletDriver.Plugin.Attributes;
+using OpenTabletDriver.Plugin.Tablet;
+using System.Numerics;
+using OpenTabletDriver.Plugin;
+using OpenTabletDriver.Plugin.Logging;
+
+namespace LeftyFlip
+{
+    public class LeftyFlipFilter : IFilter
+    {
+        public Matrix3x2 rotationMatrix = Matrix3x2.CreateRotation((float)Math.PI)
+        * Matrix3x2.CreateTranslation(Info.Driver.TabletIdentifier.MaxX, Info.Driver.TabletIdentifier.MaxY);
+        
+        public Vector2 Filter(Vector2 point)
+        {
+            Log.Write("Filter",Vector2.Transform(point,this.rotationMatrix).ToString(),LogLevel.Debug);
+            return Vector2.Transform(point,this.rotationMatrix);
+        }
+        public FilterStage FilterStage => FilterStage.PreTranspose;
+    }
+}
